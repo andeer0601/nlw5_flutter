32c246cdba8f6659542eb0155df039f3cf737b3b
Anderson <48699535+andeer0601@users.noreply.github.com>
2021-05-02 11:00:31 -0300

    part 2

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,55 @@
+﻿import 'package:DevQuiz/core/app_gradients.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/widgets.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget() : super(
+    preferredSize: Size.fromHeight(250),
+    child: Container(
+      height: 250,
+      child: Stack(
+        children: [
+          Container(
+            height: 161,
+            width: double.maxFinite,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            decoration: BoxDecoration(gradient: AppGradients.linear),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text.rich( 
+                  TextSpan(
+                    text: "Olá, ", 
+                    style: AppTextStyles.title,
+                    children: [
+                      TextSpan(
+                        text: "Anderson", 
+                        style: AppTextStyles.titleBold
+                      )
+                    ]
+                  ),
+                ),
+                Container(
+                  width: 58,
+                  height: 58,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage( 
+                      image: NetworkImage("https://media-exp1.licdn.com/dms/image/C4D03AQFbxpt679NYkw/profile-displayphoto-shrink_200_200/0/1615669888024?e=1625097600&v=beta&t=3SgGGBlFLVANqfzJeF6wM6CnpZZjcjRcjblviIfFjvU"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment(0.0, 1.0),
+            child: ScoreCardWidget()
+          ),
+        ],
+      ),
+    ),
+  );
+
+}
